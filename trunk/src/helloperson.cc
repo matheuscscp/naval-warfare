9bbcbd010dcb8b80c61b7289a5e820cf30c7e60d
leonardo.guilherme@gmail.com <leonardo.guilherme@gmail.com@5fcef849-5edc-6a78-720f-23ae332ea62c>
2012-05-11 20:43:37 +0000

    Adicionando componente greeter/helloperson e estrutura de cmakelists.txt

---
+++ trunk/src/helloperson.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "gear2d.h"
+using namespace gear2d;
+using namespace std;
+ 
+class unidade : public component::base {
+	public:
+		// constructor and destructor
+		unidade() { }
+		virtual ~unidade() { }
+		
+		// greeter family
+		virtual gear2d::component::family family() { return "greeter"; }
+		
+		// helloperson type
+		virtual gear2d::component::type type() { return "helloperson"; }
+		
+		// setup phase, to initialize paramters and other stuff
+		virtual void setup(object::signature & sig) {
+			// initialize the parameter person
+			// using the object signature, default
+			// to "Anonymous"
+			init<string>("person", sig["person"], "Anonymous");
+			
+			// initialize/writes the parameter "greetedtimes" to hold 0
+			write("greetedtimes", 0);
+		}			
+		
+		virtual void update(timediff dt) {
+			// add 1 to the number of times we're greeted someone
+			add("greetedtimes", 1);
+			
+			// Print how many times we've greeted someone
+			std::cout << "Hello, " << read<string>("person") << "! ";
+			std::cout << "I have greeted you " << read<int>("greetedtimes") << " times already!" << std::endl;
+		}
+};
+
+// the build function
+extern "C" { component::base * build() { return new unidade(); } }
