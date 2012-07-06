083c330ea029fb8558d5b1dad6d92cc1a95001f7
guilherme.rcosta@gmail.com <guilherme.rcosta@gmail.com@5fcef849-5edc-6a78-720f-23ae332ea62c>
2012-07-06 06:30:30 +0000

    loot.yaml loot.cc     - Criada classe para o loot barcogrande.yaml barcomedio.yaml barcopequeno.yaml     - Adicionadas teclas para hook de destuir o barco
    barco.cc
        - Adicionado hook de matar barco
        - Adicionado hook de life
        
    CMakeLists.txt
        - Adicionado modulo de loot
    
    chest.png
        - Adicionada imagem para o loot

---
+++ src/loot.cc
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "gear2d.h"
+using namespace gear2d;
+using namespace std;
+
+
+class loot : public component::base {
+	private:
+		gear2d::link<int> cash;
+		gear2d::link<float> x, y, w, h;
+
+	public:
+		// constructor and destructor
+		loot() {
+			loots.push_back(this);
+		}
+		virtual ~loot() {
+			loots.remove(this);
+		}
+		
+		gear2d::link<int> player;
+		
+		virtual gear2d::component::family family() { return "unit"; }
+
+		virtual gear2d::component::type type() { return "loot"; }
+
+		virtual std::string depends() { return "renderer/renderer collider/collider2d keyboard/keyboard spatial/space2d"; }
+
+		virtual void setup(object::signature & sig) {
+			initialize();
+			init<int>("cash"        , sig["cash"]        , 0);
+			cout << "Barco virou loot" << endl;
+
+
+		}
+		virtual void update(timediff dt) {
+
+		}
+		
+		
+	private:
+		static std::list<loot *> loots;
+		static bool initialized;
+
+		
+	private:
+		static void initialize() {
+			if (initialized) return;
+			initialized = true;
+		}
+};
+
+std::list<loot *> loot::loots;
+bool loot::initialized = false;
+
+// the build function
+extern "C" { component::base * build() { return new loot(); } }
