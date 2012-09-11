fe8e6f8664f1637010e603c08a1be70b8f9dbb58
matheuscscp@gmail.com <matheuscscp@gmail.com@5fcef849-5edc-6a78-720f-23ae332ea62c>
2012-09-11 01:09:08 +0000

    Fiz a tela de creditos

--- src/mainmenu.cc
+++ src/mainmenu.cc
@@ -6,12 +6,15 @@ using namespace std;
 class mainmenu : public component::base {
 	private:
 		// private vars
-		
+		component::base* credits;
+		bool credits_freshspawn;
 		
 	public:
 		// constructor and destructor
-		mainmenu() {
-			
+		mainmenu() :
+		credits(0),
+		credits_freshspawn(false)
+		{
 		}
 		
 		virtual ~mainmenu() {
@@ -22,10 +25,11 @@ class mainmenu : public component::base {
 
 		virtual gear2d::component::type type() { return "mainmenu"; }
 
-		virtual std::string depends() { return "menumanager/singleselectmanager"; }
+		virtual std::string depends() { return "keyboard/keyboard menumanager/singleselectmanager"; }
 
 		virtual void setup(object::signature & sig) {
 			hook("menu.trigger");
+			hook("key.return");
 		}
 		
 		virtual void update(timediff dt) {
@@ -33,9 +37,25 @@ class mainmenu : public component::base {
 		}
 		
 		virtual void handle(parameterbase::id pid, component::base * last, object::id owns) {
-			string opt = read<string>("menu.focus");
-			if (opt == "newgame")
-				load("partida");
+			if (pid == "menu.trigger") {
+				string opt = read<string>("menu.focus");
+				if (opt == "newgame") {
+					load("partida");
+				} else if (opt == "credits") {
+					if (!credits) {
+						credits_freshspawn = true;
+						credits = spawn("creditos")->component("spatial");
+					}
+				}
+			} else if (pid == "key.return") {
+				if (credits_freshspawn) {
+					credits_freshspawn = false;
+				}
+				else if ((credits) && (read<int>("key.return") == 1)) {
+					credits->destroy();
+					credits = 0;
+				}
+			}
 		}
 		
 	private:
